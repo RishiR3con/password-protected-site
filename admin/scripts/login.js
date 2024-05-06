13d28fca4269ad5ea9f42066fbab41925aa4ac5d
RishiR3con <149410522+RishiR3con@users.noreply.github.com>
2024-05-06 15:44:59 -0500

    Update login.js

--- admin/scripts/login.js
+++ admin/scripts/login.js
@@ -1,6 +1,6 @@
 function check(form) {
     username = new Array("admin");
-    password = new Array("admin");
+    password = new Array("admin1234");
 
 	if(form.userid.value == username[0] && form.pswrd.value == password[0]) {
 		location.replace('cookie.html')/* The Administrator homepage */
